40fe4c2f00eec42cdcfc27af36ebbc201ebd2cec
Curtis Dean <curtis.dean@pilytix.com>
2021-12-19 16:53:46 -0600

    Added functionality to only push certain sheets to Google.

--- weekly_loot_input.py
+++ weekly_loot_input.py
@@ -9,6 +9,20 @@ from functions import *
 # Get user input to determine if in testing or production mode
 mode = input('(t)est or (p)roduction?')
 
+# Set flags to determine sections that run
+pull = True
+push = {
+    'Druid': False,
+    'Hunter': False,
+    'Mage': False,
+    'Paladin': True,
+    'Priest': True,
+    'Rogue': True,
+    'Shaman': True,
+    'Warlock': True,
+    'Warrior': True
+}
+
 if mode == 't':
     spread = '1P8GQ7gf2hfSnH_C8Y2M_jf2AxjHvJG4Xxx5Si_liJQk'
 elif mode == 'p':
@@ -18,6 +32,9 @@ else: sys.exit('Production mode must be "p" or "t"')
 # Import personal email from environment vars
 email = os.environ.get('pers_email')
 
+# Set number of seconds between loot entry iterations
+sleep_time = 300
+
 # List of items not tracked by Loot List
 bad_items = ['Staff of Disintegration', 'Warp Slicer', 'Cosmic Infuser',
         'Phaseshift Bulwark', 'Devastation', 'Infinity Blade',
@@ -25,12 +42,6 @@ bad_items = ['Staff of Disintegration', 'Warp Slicer', 'Cosmic Infuser',
         'Pit Lord\'s Satchel', 'Pattern: Belt of Deep Shadow',
         'Pattern: Belt of the Black Eagle', 'Nether Vortex',
         'Pattern: Boots of the Crimson Hawk']
-
-# Create dict to store class sheets
-class_dfs = {'Druid': None, 'Hunter': None, 'Mage': None, 'Paladin': None, 'Priest': None, 'Rogue': None, 'Shaman': None, 'Warlock': None, 'Warrior': None}
-
-# Set number of seconds between loot entry iterations
-sleep_time = 300
 
 # Collect and organize loot data from previous raid
 raid_date, raid_date_str = get_raid_date()
@@ -41,12 +52,14 @@ cleaned_loot = clean_loot_df(extracted_loot, bad_items)
 # Set up gspread_pandas
 client, spread = gspread_pandas_setup(user=email, spread=spread)
 
-# Import and filter sheets from Google
-data = import_sheet(sheet='Data', index=None, header_rows=1, start_row=2, spread=spread).iloc[:,15:22]
-data = data.loc[data['Rank'] != 'Inactive'][['Player', 'Item', 'NumPassed', 'Equity']]
-data[['NumPassed', 'Equity']] = data[['NumPassed', 'Equity']].apply(pd.to_numeric)
+# Only pull sheets if flag is set to True
+if pull:
+    # Import and filter sheets from Google
+    data = import_sheet(sheet='Data', index=None, header_rows=1, start_row=2, spread=spread).iloc[:,15:22]
+    data = data.loc[data['Rank'] != 'Inactive'][['Player', 'Item', 'NumPassed', 'Equity']]
+    data[['NumPassed', 'Equity']] = data[['NumPassed', 'Equity']].apply(pd.to_numeric)
 
-roster = import_sheet(sheet='Roster', index=None, header_rows=1, start_row=1, spread=spread).iloc[:,6:10]
+    roster = import_sheet(sheet='Roster', index=None, header_rows=1, start_row=1, spread=spread).iloc[:,6:10]
 
 # Generate merged dfs
 merged_data = data.merge(roster, how='left', left_on='Player', right_on='Name')[['Player', 'Class', 'Item', 'NumPassed', 'Equity']]
@@ -67,9 +80,14 @@ loot_received = loot_received[['item', 'player_received', 'players_passed']]
 no_passes = loot_received.loc[loot_received['players_passed'].isna()].reset_index(drop=True)[['item', 'player_received']]
 passes = loot_received.loc[~loot_received['players_passed'].isna()].reset_index(drop=True)
 
-# Iterate over classes and run function to pull and store class sheets in dict
-for key in class_dfs.keys():
-    pull_class_sheets_from_google(class_dfs, key, spread)
+# Only pull class dicts if flag set to True
+if pull:
+    # Create dict to store class sheets
+    class_dfs = {'Druid': None, 'Hunter': None, 'Mage': None, 'Paladin': None, 'Priest': None, 'Rogue': None, 'Shaman': None, 'Warlock': None, 'Warrior': None}
+
+    # Iterate over classes and run function to pull and store class sheets in dict
+    for key in class_dfs.keys():
+        pull_class_sheets_from_google(class_dfs, key, spread)
 
 # Create a copy of dict and list of items/players passed
 items = list(passes['item'])
@@ -92,12 +110,15 @@ for item, player in zip(items, pass_player_class):
 # Loop through class dfs and upload them to Google Sheet
 for key in class_dfs.keys():
     
+
     # Warrior is always the last key, so we don't need to sleep after it to allow spreadsheet to finish its computing
     if key == 'Warrior':
         push_class_sheets_to_google(class_dfs, key, spread)
         break
 
-    push_class_sheets_to_google(class_dfs, key, spread)
-    now = datetime.datetime.fromtimestamp(time.time() + 300).strftime('%-I:%M:%S %p')
-    print(f'Next iteration begins at {now}.')
-    time.sleep(sleep_time)
+    # Only push sheet to google if flag is set to True
+    if push[key]:
+        push_class_sheets_to_google(class_dfs, key, spread)
+        now = datetime.datetime.fromtimestamp(time.time() + 300).strftime('%-I:%M:%S %p')
+        print(f'Next iteration begins at {now}.')
+        time.sleep(sleep_time)
